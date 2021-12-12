ac1f593ef86e642d79b4899a5bd686e89a1d28ef
Daniel Missud <daniel@missud.net>
2021-12-12 11:38:33 +0100

    Add give name and version of application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'guiMyRestGpio';
+
+  version: string = require( '../../package.json').version;
+  name: string = require( '../../package.json').name;
+
 }
